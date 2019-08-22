6bf5dbbb4cfa4e85834a1e12dcba540d712d6a1c
ivgomez <iv.gomez@hotmail.com>
2019-08-21 23:59:32 -0500

    first commit

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { FlatList, Text, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
+
+const ProductsOverviewScreen = props => {
+  const products = useSelector(state => state.products.availableProducts);
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={item => item.id}
+      renderItem={itemData => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {
+            props.navigation.navigate("ProductDetail", {
+              productId: itemData.item.id,
+              productTitle: itemData.item.title
+            });
+          }}
+          onAddToCart={() => {}}
+        />
+      )}
+    />
+  );
+};
+
+const styles = StyleSheet.create({});
+
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: "All products"
+};
+
+export default ProductsOverviewScreen;
